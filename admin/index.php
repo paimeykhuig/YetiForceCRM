e6471ff6d740708aa07f19d670966b110fd79fd2
paimeykhuig <tasarlasan@gmail.com>
2016-02-11 23:07:53 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
  * The Initial Developer of the Original Code is vtiger.
- * Portions created by vtiger are Copyright (C) vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger
  * All Rights Reserved.
  * Contributor(s): YetiForce.com
  ************************************************************************************/
